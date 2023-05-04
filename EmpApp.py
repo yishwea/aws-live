d0c5511cfc072f2fc1481203fb51582ca7dc63e3
Dusty890 <130476401+Dusty890@users.noreply.github.com>
2023-05-04 16:35:25 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -26,7 +26,7 @@ def home():
     return render_template('AddEmp.html')
 
 
-@app.route("/about", methods=['POST'])
+@app.route("/about", methods=['GET'])
 def about():
     return render_template('AboutUs.html')
 
